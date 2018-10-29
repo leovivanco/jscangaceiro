052429ec12ea4a08a7fa694a4eda510efb28d95c
Leonardo Vivanco <leorvivanco@gmail.com>
2018-10-29 19:55:05 -0300

    Add: Mensagem View

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,12 +1,19 @@
 class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        // recebe inicialmente, o modelo que encapsula uma lista vazia
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagemView');
+        this._mensagemView.update(this._mensagem);
+
     }
+    
     _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
@@ -16,7 +23,12 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem._texto = 'Negociação adicionada com sucesso'
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
+        //this._mensagemView.update(this._mensagem);
+
     }
     _criaNegociacao() {
         //	retorna	uma	instância	de	negociação
--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -8,5 +8,10 @@ class Negociacoes {
     paraArray() {
         return [].concat(this._negociacoes);
     }
-
+    get volumeTotal(){
+        return this._negociacoes
+        .reduce((total, negocicao) => 
+            total + negocicao.volume
+        , 0)
+    }
 }
